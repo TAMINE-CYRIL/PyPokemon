d1ab5b37f7f74723e5a99c4a1de014a9518d6ab4
Xanooo <tamineha@yahoo.fr>
2025-02-10 01:54:32 +0100

    On prépare la classe Pokemon, et quelques modifications mineures.

---
+++ Pokemon.py
@@ -0,0 +1,16 @@
+class Pokemon:
+    def __init__(self, name, ptype, hp, attack, defense, speed):
+        self.name = name
+        self.ptype = ptype
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            self.hp = 0
+            print(self.name, "is KO!")
+
+
--- Screen.py
+++ Screen.py
@@ -1,16 +1,13 @@
 import pygame
 
-from Trainer import Trainer
-
-
 class Screen:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Pygame RPG")
+        pygame.display.set_caption("PyPokemon")
         self.clock = pygame.time.Clock()
         self.running = True
-        self.framerate = 60;
+        self.framerate = 60
 
     def update(self):
         pygame.display.flip()
